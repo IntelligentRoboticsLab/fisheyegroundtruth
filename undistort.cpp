5754727365ce00d86ad6a70e9aaa22cac3db4c8f
Eugenio Bargiacchi <svalorzen@gmail.com>
2014-02-10 15:05:54 +0100

    Pseudocode for field undistortion

---
+++ undistort.cpp
@@ -0,0 +1,13 @@
+
+
+int main() {
+// Step 0: Initialize stuff
+// Step 1: Load image
+// Step 2: Find green field / Color stuff
+// Step 3: Transform what we think is the field into a rectangle
+// Done for now.
+// Step 2: Initialize
+
+
+    return 0;
+}
