fac66449e1b409df8daa431d48d1136dfcc8e9ff
Auke <wiggers.auke@gmail.com>
2014-02-10 19:54:09 +0100

    Selector now with filter etc

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-g++ -std=c++11 selector.cpp -lopencv_core -lopencv_highgui -Wl,-rpath -Wl,.
+g++ -std=c++11 selector.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -Wl,-rpath -Wl,/usr/local/lib
 ./a.out test_data.png
--- selector.cpp
+++ selector.cpp
@@ -32,16 +32,68 @@ void mouseHandler(int event, int x, int y, int flags, void* param) {
         img1 = frame.clone();
 
         auto mousePoint = cv::Point(x, y);
-        std::cout << "Released at: " << mousePoint << '\n';
-        std::cout << "Range is from " << point << " to " << mousePoint << '\n';
+        //std::cout << "Released at: " << mousePoint << '\n';
+        //std::cout << "Range is from " << point << " to " << mousePoint << '\n';
 
         if ( point.x > mousePoint.x ) std::swap(point.x, mousePoint.x);
         if ( point.y > mousePoint.y ) std::swap(point.y, mousePoint.y);
 
-        auto roi = img1(cv::Range(point.y, mousePoint.y), cv::Range(point.x, mousePoint.x));
         // Perform actual work
-        bitwise_not(roi, roi);
+        cv::cvtColor(img1, img1, CV_BGR2HSV);
 
+        // Replace some value in selected square
+        cv::Mat hsv[3];
+        cv::split(img1, hsv);
+        hsv[2].setTo(255);   // TODO remove this
+        cv::merge(hsv, 3, img1);
+        auto roi = img1(cv::Range(point.y, mousePoint.y), cv::Range(point.x, mousePoint.x));
+
+        // Use histograms
+        int hbins = 30, sbins = 32;
+        int histSize[] = {hbins, sbins};
+
+        float hranges[] = { 0, 180 };
+        float sranges[] = { 0, 256 };
+        const float* ranges[] = { hranges, sranges };
+        cv::MatND hist;
+        int channels[] = {0, 1};
+
+
+        calcHist(   &roi, 1, channels, cv::Mat(), // do not use mask
+                    hist, 2, histSize, ranges,
+                    true, // the histogram is uniform
+                    false );
+        double maxVal=0;
+        minMaxLoc(hist, 0, &maxVal, 0, 0);
+
+        int scale = 10;
+        cv::Mat histImg = cv::Mat::zeros(sbins*scale, hbins*scale, CV_8UC3);
+
+        for( int h = 0; h < hbins; h++ )
+            for( int s = 0; s < sbins; s++ )
+            {
+                float binVal = hist.at<float>(h, s);
+                int intensity = std::round(binVal*255/maxVal);
+                cv::rectangle( histImg, cv::Point(h*scale, s*scale),
+                               cv::Point( (h+1)*scale - 1, (s+1)*scale - 1),
+                               cv::Scalar::all(intensity), CV_FILLED );
+            }
+
+        cv::namedWindow( "H-S Histogram", 1 );
+        cv::imshow( "H-S Histogram", histImg );
+
+        // Colorfilter
+        int h_min, s_min, h_max, s_max;
+        h_min = 15; h_max = 256; s_min = 16; s_max = 256;
+        cv::Mat imgResult(img1.size(), IPL_DEPTH_8U, 3);
+        cv::Scalar min_color = CV_RGB(h_min, s_min, 254);
+        cv::Scalar max_color = CV_RGB(h_max, s_max, 256);
+        cv::inRange(img1, min_color, max_color, imgResult);
+
+        cv::cvtColor(img1, img1, CV_HSV2BGR);
+        cv::rectangle(img1, point, mousePoint, CV_RGB(255, 0, 0));
+        cv::namedWindow("asdasd");
+        imshow("asdasd", imgResult);
         imshow("result", img1);
         drag = 0;
     }
